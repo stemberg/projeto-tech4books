1dbf316740f3ece1d092ac28740b69ca68ff9565
Stemberg Santos <stembergsantos@gmail.com>
2022-02-23 22:14:39 -0300

    ultimas alterações do projeto tech4books do dia 23/02

--- src/main/java/br/com/tech4me/tech4books/controller/LivroController.java
+++ src/main/java/br/com/tech4me/tech4books/controller/LivroController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +49,22 @@ public class LivroController {
     public ResponseEntity<Livro> cadastrarLivro(@RequestBody Livro livro){
 
         return new ResponseEntity<>(repositorio.save(livro), HttpStatus.CREATED);
+        
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Livro> excluirLivro(@PathVariable String id){
+
+        repositorio.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Livro> atualizarLivro(@PathVariable String id, @RequestBody Livro livro) {
+        
+        livro.setId(id);
+        return new ResponseEntity<>(repositorio.save(livro), HttpStatus.ACCEPTED);
         
     }
 
